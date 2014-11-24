4b9a1af90bc0e1674130f43c2a4f25c407c1e18f
James Blashill <james@blashill.com>
2014-11-24 14:24:25 -0500

    Added date-picker directive

---
+++ date-picker/date-picker.js
@@ -0,0 +1,32 @@
+var app = angular.module('myApp', []);
+
+app.controller('MyController', function($scope) {
+  $scope.doSomethingCool = function() {
+    alert('kaboom!');
+  };
+});
+
+app.directive('datePicker', function() {
+  return {
+    restrict: "E",
+    require: "ngModel",
+    replace: true,
+    template: "<input type='text'>",
+    link: function(scope, $el, attrs, ngModel) {
+      var callbackExpression = attrs.onClose;
+
+      $el.datepicker({
+        onClose: function(newValue) {
+          scope.$eval(callbackExpression);
+          scope.$apply(function() {
+            ngModel.$setViewValue(newValue);
+          });
+        }
+      });
+
+      ngModel.$render = function() {
+        $el.datepicker( "setDate", ngModel.$modelValue )
+      };
+    }
+  }
+});
