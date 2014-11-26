be85a391eba67cc6fd7270927c3390f3d5b0def4
James Blashill <james@blashill.com>
2014-11-26 16:15:43 -0500

    Added transclusion + template file to hello-world

--- hello-world/hello-world.js
+++ hello-world/hello-world.js
@@ -2,6 +2,10 @@ angular.module('myApp')
   .directive('helloWorld', function() {
     return {
       restrict: "E",
-      template: "<h1>Hello world!</h1>"
+      templateUrl: "hello.html",
+      transclude: true,
+      scope: {
+        name: "@"
+      }
     }
   });
